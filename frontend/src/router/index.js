cbba8f4d4ca730cde9d62ddc90a0dd0d50b780f3
celinevs <csetiadi22@students.calvin.ac.id>
2024-12-08 13:24:41 +0700

    tambahkan rute index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import workspace from '@/components/workspace.vue'
+import form from '@/components/form.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     path:'/workspace',
     name:'workspace',
     component:workspace,
+  },
+  {
+    path:'/form',
+    name:'form',
+    component:form,
   }
     
   ],
